111dfd6f68ac0ccb864d324e3fd384f8ae5f47d8
Ella <ellacalugay@gmail.com>
2023-07-02 22:06:52 +0800

    Prompts the user to enter the name, type, and age of his or her pet.

--- PetMain.py
+++ PetMain.py
@@ -2,5 +2,9 @@
 
 # Pseudocode
 # Prompts the user to enter the name, type, and age of his or her pet.
+name = input("Enter the name of your pet: ")
+animal_type = input("Enter the type of your pet: ")
+age = input("Enter the age of your pet: ")
+
 # Create an object of the Pet class
 # Display the entered information using the accessor methods
