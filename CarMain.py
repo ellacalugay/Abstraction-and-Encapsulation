9597c92244504dc386a2dd08ae6660c289d8792e
Ella <ellacalugay@gmail.com>
2023-07-02 21:53:42 +0800

    Create a car object

--- CarMain.py
+++ CarMain.py
@@ -1,6 +1,8 @@
 # Ella Lureen C. Calugay | BSCPE 1-5 | Assignment #9 | Abstraction and Encapsulation
 
 # Pseudocode
+from Car import Car
 # Create a Car object
+my_car = Car(2022, "Bugatti")
 # Call the accelerate method five times and display the current speed
 # Call the brake method five times and display the current speed
