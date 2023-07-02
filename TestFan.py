d3eb02c0707c591ee2f5c57c2eded0b246171652
Ella <ellacalugay@gmail.com>
2023-07-02 21:36:09 +0800

    Creat a class named TestFan

--- TestFan.py
+++ TestFan.py
@@ -2,6 +2,7 @@
 
 # Pseudocode
 # Create a class that creates two Fan Objects.
+class TestFan:
 # For the first object, assign the maximum speed, radius 10, color yellow, and turn it on.
 # For the second object, assign the medium speed, radius 5, color blue, and turn it off.
 # Display the first object's speed, radius, color, and on properties.
