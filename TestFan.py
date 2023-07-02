2ce2c6df0706f3215925d08f9ed707b46a3318eb
Ella <ellacalugay@gmail.com>
2023-07-02 21:16:18 +0800

    Pseudocode for TestFan

---
+++ TestFan.py
@@ -0,0 +1,9 @@
+# Ella Lureen C. Calugay | BSCPE 1-5 | Assignment #9 | Abstraction and Encapsulation
+
+# Pseudocode
+# Create a class that creates two Fan Objects.
+# For the first object, assign the maximum speed, radius 10, color yellow, and turn it on.
+# For the second object, assign the medium speed, radius 5, color blue, and turn it off.
+# Display the first object's speed, radius, color, and on properties.
+# Display the second object's speed, radius, color, and on properties.
+# Create an instance of TestFan to run the program
