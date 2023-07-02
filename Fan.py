3fdf66b9a34fbcde913f3c4d5e254cf35e6bdeaf
Ella <ellacalugay@gmail.com>
2023-07-02 20:55:54 +0800

    Pseudocode

--- Fan.py
+++ Fan.py
@@ -1,1 +1,11 @@
 # Ella Lureen C. Calugay | BSCPE 1-5 | Assignment #9 | Abstraction and Encapsulation
+
+# Pseudocode
+# Create a class named Fan to represent a fan.
+# Create three constants named SLOW, MEDIUM, and FAST with the values 1, 2, and 3 to denote the fan speed.
+# Create a constructor that creates a fan with the specified speed (default SLOW), radius (default 5), color (default blue), and on (default False).
+# A private int data field named speed that specifies the speed of the fan.
+# A private bool data field named on that specifies whether the fan is on (the default is False).
+# A private float data field named radius that specifies the radius of the fan.
+# A private string data field named color that specifies the color of the fan.
+# The accessor(getters) and mutator(setters) methods for all four data fields.
