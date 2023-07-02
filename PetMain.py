e1575916ba663175f9407fbafe23016ed44e9a85
Ella <ellacalugay@gmail.com>
2023-07-02 22:06:10 +0800

    Pseudocode

---
+++ PetMain.py
@@ -0,0 +1,6 @@
+# Ella Lureen C. Calugay | BSCPE 1-5 | Assignment #9 | Abstraction and Encapsulation
+
+# Pseudocode
+# Prompts the user to enter the name, type, and age of his or her pet.
+# Create an object of the Pet class
+# Display the entered information using the accessor methods
