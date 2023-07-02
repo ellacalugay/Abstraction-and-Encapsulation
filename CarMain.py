d58045ef8e08fe00a86b6b63a86aa094ed267ca1
Ella <ellacalugay@gmail.com>
2023-07-02 21:55:00 +0800

    Call the accelerate method five times and display the current speed

--- CarMain.py
+++ CarMain.py
@@ -2,7 +2,14 @@
 
 # Pseudocode
 from Car import Car
+
 # Create a Car object
 my_car = Car(2022, "Bugatti")
+
 # Call the accelerate method five times and display the current speed
+print("--Accelerate--")
+for i in range(1, 6):
+    my_car.accelerate()
+    print(f"[{i}] Current speed of the car:", my_car.get_speed())
+
 # Call the brake method five times and display the current speed
