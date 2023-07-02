72a21d6ee3f1bbda2be7d78498ea2c0446b45470
Ella <ellacalugay@gmail.com>
2023-07-02 21:52:52 +0800

    Pseudocode

---
+++ CarMain.py
@@ -0,0 +1,6 @@
+# Ella Lureen C. Calugay | BSCPE 1-5 | Assignment #9 | Abstraction and Encapsulation
+
+# Pseudocode
+# Create a Car object
+# Call the accelerate method five times and display the current speed
+# Call the brake method five times and display the current speed
