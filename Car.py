b314c402aba7de8815fa044044c3f75f41c95441
Ella <ellacalugay@gmail.com>
2023-07-02 21:49:39 +0800

    Create a class named Car and create its constructor

--- Car.py
+++ Car.py
@@ -2,7 +2,13 @@
 
 # Pseudocode
 # Create a class named Car
+class Car:
 # Create a constructor
+    def __init__(self, year_model=2022, make='La Voiture Noire', speed=0):
+        self.__year_model = year_model
+        self.__make = make
+        self.__speed = speed
+        
 # Create accelerate methd that add 5 to the speed data attribute each time it is called
 # Create break methd that subtract 5 to the speed data attribute each time it is called
 # Create get_speed method that should return the current speed.
