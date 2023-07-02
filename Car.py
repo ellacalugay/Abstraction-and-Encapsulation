9d69e813df6e7b416f4abea48a0f3df5874018ce
Ella <ellacalugay@gmail.com>
2023-07-02 21:46:37 +0800

    Pseudocode

---
+++ Car.py
@@ -0,0 +1,8 @@
+# Ella Lureen C. Calugay | BSCPE 1-5 | Assignment #9 | Abstraction and Encapsulation
+
+# Pseudocode
+# Create a class named Car
+# Create a constructor
+# Create accelerate methd that add 5 to the speed data attribute each time it is called
+# Create break methd that subtract 5 to the speed data attribute each time it is called
+# Create get_speed method that should return the current speed.
