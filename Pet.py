271c03f3ccd4a604e2cb7ec9fa8b7ec7723b02a5
Ella <ellacalugay@gmail.com>
2023-07-02 21:58:20 +0800

    Pseudocode

---
+++ Pet.py
@@ -0,0 +1,11 @@
+# Ella Lureen C. Calugay | BSCPE 1-5 | Assignment #9 | Abstraction and Encapsulation
+
+# Pseudocode
+# Create a class named Pet
+# Create a constructor
+# Create a set name method
+# Create a set animal_type method
+# Create a set age method
+# Create a get name method
+# Create a get animal type method
+# Create a get age method
